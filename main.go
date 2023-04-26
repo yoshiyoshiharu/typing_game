86959a3e4ce7a0bb8d4f2b27e14e2c989e1d7b42
yoshiyoshiharu <haruki1226yh@ezweb.ne.jp>
2023-04-26 21:28:50 +0900

    点数表示

--- main.go
+++ main.go
@@ -3,8 +3,19 @@ package main
 import "github.com/yoshiyoshiharu/typing_game/typing"
 
 func main() {
+	var is_correct bool
+	var point int;
+
 	for {
 		title := "aaa"
-		println(typing.Judge(title))
+		println("title: ", title)
+
+		is_correct = typing.Judge(title)
+
+		if is_correct {
+			point++
+		}
+		println("point: ", point)
 	}
+
 }
